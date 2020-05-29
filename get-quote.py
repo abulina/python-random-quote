5de4bdb776bed941c39100ac33c091acd157a95b
Jamel Achahbar <jamel.achahbar@gmail.com>
2020-05-29 15:50:05 +0200

    added randomization

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 #def sympa():
 #   print("Keep it logically awesome.")
-
+import random
 f = open("quotes.txt")
 quotes = f.readlines()
 f.close()
-
-print(quotes[-1])
+#Note If you want to add or remove quotes from your text file, you could change the last variable to update automatically:
+#last = len(quotes) - 1
+last = 13
+rnd = random.randint(0, last)
+print(quotes[rnd])
 
 #if __name__== "__main__":
 #  sympa()
